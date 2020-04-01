75cb6d14afb6d0036ac54286987f9b4789531191
Satwik <satwikkansal@gmail.com>
2020-04-01 18:18:40 +0530

    Update game environment

--- environment.py
+++ environment.py
@@ -1,6 +1,8 @@
 import gym
 
-from gym import spaces
+from gym.spaces import Tuple, Discrete
+
+from game_short import AvalonGame
 
 """
 Game state space = Info necessary to make a decision, which includes:
@@ -27,11 +29,15 @@ Action to take
 """
 Regarding legal actions: https://github.com/openai/gym/issues/413 
 Tuple of discrete action spaces: https://github.com/openai/gym/blob/master/gym/envs/algorithmic/algorithmic_env.py#L78 and https://github.com/bzier/gym-mupen64plus/blob/3551409db59ccba1912c004c1612b2b1aa2ecc8d/gym_mupen64plus/envs/MarioKart64/discrete_envs.py#L5
+Multi discrete action space discussion: https://github.com/openai/universe-starter-agent/issues/75
 
 Ideas: To begin with
 
 - Pass the legal actions info in the info object (needs to be implemented in reset and step method), use that info in the agent
 - Make it more strict by removing the legal actions and penalizing the agent for taking non-legal actions
+
+
+https://github.com/openai/gym/blob/master/gym/envs/toy_text/blackjack.py
 """
 
 
@@ -41,11 +47,24 @@ class AvalonEnv(gym.Env):
     def __init__(self, num_players):
         super(AvalonEnv, self).__init__()
 
-        self.action_space = []
-        self.observation_space = []
+        self.game = AvalonGame(num_players)
+        self.action_space = self.action_space = Tuple(
+            [Discrete(3), Discrete(3), Discrete(2)]
+        )
+        self.player_types = [p.char_type.val for p in self.game.players]
+
+        self.observation_space = Tuple((
+            Discrete(self.game.num_players),  # Character types
+            Discrete(1),  # Quest
+            Discrete(1),  # Proposal
+            Discrete(1),  # Leader
+            #Discrete(self.game.max_quests),  # Current quest
+            #Discrete(self.game.max_proposals_allowed),  # Current proposal
+        ))
 
     def step(self, action):
-        pass
+        assert self.action_space.contains(action)
+        return self._next_observation()
 
     def reset(self):
         # Reset all the stuff
@@ -57,7 +76,7 @@ class AvalonEnv(gym.Env):
         pass
 
     def _next_observation(self):
-        obs = []
+        obs = [self.player_types, self.game.current_quest, self.game.current_proposal_number, self.game.current_leader]
         return obs
 
     def _take_action(self, action):
