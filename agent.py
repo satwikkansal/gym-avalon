bde7c0bb2b288b96d3a4a113b2d7ea2fcf61cd39
Satwik <satwikkansal@gmail.com>
2020-04-26 17:31:08 +0530

    Add comments for agents

--- agent.py
+++ agent.py
@@ -2,8 +2,6 @@ from collections import defaultdict
 import numpy as np
 import random
 
-from game.enums import ActionType
-
 
 class Agent:
     def __init__(self, env):
@@ -12,16 +10,69 @@ class Agent:
     def get_next_action(self, obs, reward, info):
         raise NotImplementedError
 
+    def _sample(self, total_players, players_to_choose):
+        """
+        Custom sample function. The action space for team
+        selection is MultiBinary(5) but the validity of actions
+        depend on the players_to_choose (Ex [1 1 1 1 1] i.e. all 5 players in team
+        is an invalid action if we need only 3 players), that's why a custom function
+        that can give sample actions that are also valid.
+        """
+        action_space = self.env.action_space
+        return tuple(
+            [
+                self._get_sample_team_selection_action(total_players, players_to_choose),
+                action_space.spaces[1].sample(),
+                action_space.spaces[2].sample()
+            ]
+        )
+
+    def _get_sample_team_selection_action(self, total_players, players_to_choose):
+        # Create an array of zeroes and ones indicating selection
+        arr = np.array([0] * (total_players - players_to_choose) + [1] * players_to_choose)
+        # Shuffle inplace
+        np.random.shuffle(arr)
+        return arr
+
 
 class RandomAgent(Agent):
     def __init__(self, **kwargs):
         super(RandomAgent, self).__init__(**kwargs)
 
     def get_next_action(self, obs, reward, info):
-        return self.env.action_space.sample() # TODO: Use custom sample function
+        return self._sample(info['num_players'], info['quest_team_size'])
 
 
 class QTableAgent(Agent):
+    """
+    Every Character type has it's own set of q-values since the dynamics of games
+    are different for different character types.
+
+    The structure of the q_table in this class is as follows
+
+    {
+        char_type_1: {
+            state_1: {
+                action_1: q_value,
+                action_2: q_value
+            },
+            state_2: {
+                action_1: q_value,
+                action_2: q_value
+            },
+        },
+        char_type_2: {
+            state_1: {
+                action_1: q_value,
+                action_2: q_value
+            },
+            state_2: {
+                action_1: q_value,
+                action_2: q_value
+            },
+        }
+    }
+    """
     def __init__(self, alpha=0.1, gamma=0.6, epsilon=0.2, **kwargs):
         super(QTableAgent, self).__init__(**kwargs)
         self.q_table = {}
@@ -31,34 +82,17 @@ class QTableAgent(Agent):
 
     @staticmethod
     def _init_q_table_for_char_type():
+        # Default value of 0 for every state-action pair
         return defaultdict(lambda: defaultdict(float))
 
     def get_q_table(self, char_type):
+        """
+        Helper method to get Q-table for a character type. Creates one if doesn't exists
+        already.
+        """
         if char_type not in self.q_table:
             self.q_table[char_type] = self._init_q_table_for_char_type()
         return self.q_table[char_type]
-
-    def _sample(self, total_players, players_to_choose):
-        """
-        #TODO: Move to super class
-        :param total_players:
-        :param players_to_choose:
-        :return:
-        """
-        action_space = self.env.action_space
-        return tuple(
-            [
-                self._get_sample_team_selection_action(total_players, players_to_choose),
-                #action_space.spaces[0].sample(),
-                action_space.spaces[1].sample(),
-                action_space.spaces[2].sample()
-            ]
-        )
-
-    def _get_sample_team_selection_action(self, total_players, players_to_choose):
-        arr = np.array([0] * (total_players - players_to_choose) + [1] * players_to_choose)
-        np.random.shuffle(arr)
-        return arr
 
     def get_next_action(self, new_obs, reward, info):
         q_table = self.get_q_table(info['char_type'])
@@ -66,7 +100,9 @@ class QTableAgent(Agent):
         action_taken = info['prev_action']
 
         if action_taken:
+            # Team selection Action needs to be casted to a tuple to store in a dictionary
             action_taken = (tuple(action_taken[0]), action_taken[1], action_taken[2])
+            # Updating the q_values as per bellman equation
             old_value = q_table[old_obs][action_taken]
             next_max = max(q_table[new_obs].values()) if len(q_table[new_obs].values()) > 0 else 0.0
             new_value = (1 - self.alpha) * old_value + self.alpha * (reward + self.gamma * next_max)
@@ -75,10 +111,14 @@ class QTableAgent(Agent):
         return self._select_next_action(q_table, new_obs, info)
 
     def _select_next_action(self, q_table, obs, info):
+        """
+        Select next action based on epsilon-greedy method.
+        TODO: Use better strategy for exploration than the epsilon-greedy method.
+        """
         random_val = random.uniform(0, 1)
         random_action = self._sample(info['num_players'], info['quest_team_size'])
         if random_val < self.epsilon:
-            action =  random_action # Explore action space
+            action = random_action  # Explore action space
         else:
             q_vals = q_table[obs]
             action = max(q_vals, key=q_vals.get, default=random_action)  # Exploit learned values
