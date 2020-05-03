5db8541fe942a43ac1b4d20f0f270e6086a728bb
Satwik <satwikkansal@gmail.com>
2020-05-03 17:33:48 +0530

    Use multidiscrete spaces instead of tuple

--- environment.py
+++ environment.py
@@ -1,8 +1,8 @@
 import gym
+from gym.spaces import Tuple, Discrete, MultiBinary, MultiDiscrete
+import numpy as np
 
-from gym.spaces import Tuple, Discrete, MultiBinary
-
-from game.enums import ActionType, PlayerVisibility, Team
+from game.enums_and_config import ActionType, PlayerVisibility, Team, team_selection_move_map
 from game.avalon import AvalonGame
 
 """
@@ -52,6 +52,9 @@ class AvalonEnv(gym.Env):
         self.num_players = num_players
         self._initialize_game(num_players, enable_logs)
 
+
+        # Commenting because Tuple space isn't supported by many models.
+        """
         # See action_value_map for the behavior of various action values
         self.action_space = Tuple(
             [
@@ -64,10 +67,21 @@ class AvalonEnv(gym.Env):
         self.observation_space = Tuple((
             Discrete(len(PlayerVisibility)),  # Explained in the top comments
             Discrete(len(ActionType)),  # Action type
-            Discrete(1),  # Quest
-            Discrete(1),  # Proposal
-            Discrete(1),  # Leader
+            Discrete(5),  # Quest
+            Discrete(5),  # Proposal #TODO: Fixen(team_selection_move_map[self.num_players])
+            Discrete(5),  # Leader
         ))
+        """
+
+        # Use MultiDiscrete action and observation spaces instead
+        self.action_space = MultiDiscrete([len(team_selection_move_map[self.num_players]), 3, 3])
+
+        obs_space = [len(PlayerVisibility)] * self.num_players
+        obs_space += [len(ActionType), self.game.max_quests, self.num_players, self.num_players]
+
+        self.observation_space = MultiDiscrete(
+            obs_space
+        )
 
     def _initialize_game(self, num_players, enable_logs):
         self.game = AvalonGame(num_players, enable_logs=enable_logs)
@@ -100,6 +114,8 @@ class AvalonEnv(gym.Env):
 
         # Pack into an observation
         # Note this structure should conform to the one defined in self.observation_space
+        # Commenting this because no longer using Tuple spaces
+        """
         obs = (
             tuple(visibilities),
             feedback.action_type.value,
@@ -107,6 +123,12 @@ class AvalonEnv(gym.Env):
             feedback.proposal_number,
             feedback.leader,
         )
+        """
+        # Adjusting as per MultiDiscrete space
+        obs = np.concatenate([
+             visibilities,
+            [feedback.action_type.value, feedback.quest_number, feedback.proposal_number, feedback.leader]
+            ])
 
         # Extra Info that needs to be passed to agent to decide stuff.
         # This info is however not a part of state space, so will only be helpful
@@ -116,7 +138,9 @@ class AvalonEnv(gym.Env):
             'char_type': self.agent.char_type,
             'prev_action': prev_action,
             'num_players': self.game.num_players,
-            'quest_team_size': feedback.quest_team_size
+            'quest_team_size': feedback.quest_team_size,
+            'game_winner': feedback.game_winner,
+            'agent_team': self.agent.team
         }
 
         return obs, info
@@ -144,9 +168,11 @@ class AvalonEnv(gym.Env):
 
         Ideas:
         - Penalize for illegal actions
+        - Reward for legal actions
         - Mission loose results in -ve reward
         - Mission win results in +ve reward
-        - Game win / loose rewards
+        - Game win results in +ve reward
+        - Game loss results in -ve reward
         """
         reward = 0
 
@@ -168,8 +194,12 @@ class AvalonEnv(gym.Env):
 
         for at in ActionType:
             if at == ActionType.TEAM_SELECTION:
-                # Team selection is always sampled in current scenario
-                continue
+                relevant_action = self._get_relevant_value_for_action(action, action_type)
+                if relevant_action is None:
+                    # This case occurs when the agent selects invalid team (one of wrong size)
+                    reward -= 0.4
+                else:
+                    reward += 0.1
             # Invalid action supplied by agent instead of a No-OP (0 value)
             if at != action_type and action[at.value]:
                 reward -= 0.5
@@ -178,11 +208,14 @@ class AvalonEnv(gym.Env):
 
         return reward
 
-    def reset(self):
+    def reset(self, external=True):
         # Reset all the stuff
         self._initialize_game(self.num_players, self.enable_logs)
         feedback = self._get_agent_feedback()
-        return self._convert_game_feedback_to_observation(feedback, None)
+        obs, info = self._convert_game_feedback_to_observation(feedback, None)
+        if external:
+            return obs
+        return obs, info
 
     def render(self, mode='human'):
         if self.enable_logs:
@@ -209,7 +242,7 @@ class AvalonEnv(gym.Env):
         to a corresponding value that can be passed to the game.
         """
         action_value_map = {
-            ActionType.TEAM_SELECTION: None,  # The vector value should
+            ActionType.TEAM_SELECTION: team_selection_move_map[self.num_players],  # The vector value should
             ActionType.TEAM_APPROVAL: {
                 0: None,
                 1: True,
@@ -222,10 +255,12 @@ class AvalonEnv(gym.Env):
             }
         }
 
+        relevant_action = action_value_map[action_type][action[action_type.value]]
+
         if action_type is ActionType.TEAM_SELECTION:
-            relevant_action = (action[action_type.value].nonzero())[0]
-        else:
-            relevant_action = action_value_map[action_type][action[action_type.value]]
+            # relevant_action = (action[action_type.value].nonzero())[0]
+            if len(relevant_action) != self.game.current_quest.team_size:
+                relevant_action = None
 
         return relevant_action
 
