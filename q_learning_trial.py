fec62fc889fd18474df7cdf2456b13a9eded08c7
Satwik <satwikkansal@gmail.com>
2020-05-03 17:34:05 +0530

    Rename file and add callback functionality

--- q_learning_trial.py
+++ q_learning_trial.py
@@ -1,13 +1,74 @@
 from collections import deque, defaultdict
 from flatten_dict import flatten
+import pickle
+
 import numpy as np
 
 from environment import AvalonEnv
 from agent import RandomAgent, QTableAgent
-from game.enums import CharacterType, PlayerVisibility
+from game.enums_and_config import CharacterType
 
 
-def train(num_episodes, env, agent, last_n_plot=50):
+def evaluate(model, env, num_episodes):
+    reward_so_far = []
+    penalties_so_far = []
+    agent_game_results = defaultdict(list)
+
+    for _ in range(num_episodes):
+        obs = env.reset()
+        done, state = False, None
+        episode_reward = 0
+        episode_penalties = 0
+        _info = None
+
+        while not done:
+            action = model.predict(obs, _info)
+            obs, reward, done, _info = env.step(action)
+
+            # Every -ve reward counts as penalty
+            if reward < 0:
+                episode_penalties += 1
+
+            episode_reward += reward
+
+            if done: # game over
+                if type(_info) is list:
+                    info = _info[0]
+                else:
+                    info = _info
+                agent_game_results[info['char_type']].append(info['game_winner'] == info['agent_team'])
+
+        reward_so_far.append(episode_reward)
+        penalties_so_far.append(episode_penalties)
+
+    return reward_so_far, penalties_so_far, agent_game_results
+
+
+def callback(env, model, save_path, best_mean_reward, num_eval_episodes=100, target_reward=None):
+    eval_rewards, eval_penalties, eval_agent_results = evaluate(model, env, num_eval_episodes)
+    mean_eval_reward, std_eval_reward = np.mean(eval_rewards), np.std(eval_rewards)
+    print()
+    print(f"Average reward per episode {np.mean(eval_rewards)}")
+    print(f"Average penalties per episode: {np.mean(eval_penalties)}")
+    print_summary(eval_agent_results)
+
+    # New best model, you could save the agent here
+    if mean_eval_reward > best_mean_reward:
+        best_mean_reward = mean_eval_reward
+        # Saving best model
+        print(f"Saving new best model to {save_path}")
+        save(model.q_table, save_path)
+
+    if target_reward and mean_eval_reward > target_reward:
+        print(f"Achieved reward of {mean_eval_reward}, halting training")
+        print(f"Saving new best model to {save_path}")
+        save(model.q_table, save_path)
+        return False
+
+    return True
+
+
+def train(num_episodes, env, agent, last_n_plot=50, callback_every=5000):
     """
     The method to train the agent.
     :param num_episodes: number of episodes (game instances) to train the agent for
@@ -26,8 +87,15 @@ def train(num_episodes, env, agent, last_n_plot=50):
 
     agent_game_results = defaultdict(list)
 
-    for _ in range(num_episodes):
-        obs, info = env.reset()
+    best_mean_reward = -float('inf')
+
+    for curr_episode in range(num_episodes):
+        if curr_episode % callback_every == 0:
+            should_continue = callback(env, agent, "q_table.pickle", best_mean_reward, 100, 4.3)
+            if not should_continue:
+                break
+
+        obs, info = env.reset(external=False)
         info['prev_obs'] = None
         reward = 0
         done = False
@@ -73,6 +141,12 @@ def print_summary(agent_game_results):
         print(f'Agent won {win_percent}% games out of {total_games} games while taking {char_type.name} role.')
 
 
+def save(model, save_path):
+    with open(save_path, "wb") as f:
+        d = dict(model)
+        pickle.dump(d, f)
+
+
 """
 Usage,
 
@@ -83,7 +157,7 @@ present in the Player class.
 env = AvalonEnv(6, enable_logs=False, autoplay=False)
 
 agent = QTableAgent(env=env)
-num_episodes = 500
+num_episodes = 50000
 rewards, penalties, agent_game_results = train(num_episodes, env, agent)
 
 # A nice way to get the q-table and sort it by q-values
@@ -91,3 +165,4 @@ rewards, penalties, agent_game_results = train(num_episodes, env, agent)
 # A nice way to debug and add new features as well.
 servant_table = sorted(flatten(agent.q_table[CharacterType.SERVANT]).items(), key=lambda x: x[1])
 print_summary(agent_game_results)
+
